374c02f35e41c6848b5eebb422c8cd6d36501b64
novando <rizkynovando@gmail.com>
2025-06-16 08:42:55 +0700

    feat: add eslint, rename vue files

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import './styles/style.css'
-import { createApp } from 'vue'
-import { route } from './routes/route.ts'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import "./styles/style.css"
+import { createApp } from "vue"
+import { route } from "./routes/route.ts"
+import { createPinia } from "pinia"
+import App from "./App.vue"
 
 const pinia = createPinia()
 
 createApp(App)
   .use(route)
   .use(pinia)
-  .mount('#app')
+  .mount("#app")
--- src/stores/toaster.store.ts
+++ src/stores/toaster.store.ts
@@ -1,7 +1,7 @@
-import {defineStore} from 'pinia'
+import {defineStore} from "pinia"
 
 type NewToastArgType = {
-  type: 'ERR'|'WARN'|'INFO'|'SUCCESS'
+  type: "ERR"|"WARN"|"INFO"|"SUCCESS"
   title: string
   message?: any
 }
@@ -12,7 +12,7 @@ type StateType = {
   TOASTS: ToastType[]
 }
 
-export const useToaster = defineStore('toaster', {
+export const useToaster = defineStore("toaster", {
   state: (): StateType => ({
     TOASTS: [],
   }),
@@ -22,17 +22,17 @@ export const useToaster = defineStore('toaster', {
   actions: {
     newNoty(arg: NewToastArgType) {
       const id = new Date().getTime()
-      if (typeof arg.message === 'object') arg.message = Object.values(arg.message)[0]
+      if (typeof arg.message === "object") arg.message = Object.values(arg.message)[0]
       if (this.TOASTS.length > 2) this.TOASTS.shift()
       this.TOASTS.push({...arg, id})
       setTimeout(() => {
         this.TOASTS = this.TOASTS.filter((item) => item.id !== id)
       }, 3000)
     },
-    notyErr(title: string, message?: any) { this.newNoty({type: 'ERR', title, message}) },
-    notyWarn(title: string, message?: any) { this.newNoty({type: 'WARN', title, message}) },
-    notyInfo(title: string, message?: any) { this.newNoty({type: 'INFO', title, message}) },
-    notySuccess(title: string, message?: any) { this.newNoty({type: 'SUCCESS', title, message}) },
+    notyErr(title: string, message?: any) { this.newNoty({type: "ERR", title, message}) },
+    notyWarn(title: string, message?: any) { this.newNoty({type: "WARN", title, message}) },
+    notyInfo(title: string, message?: any) { this.newNoty({type: "INFO", title, message}) },
+    notySuccess(title: string, message?: any) { this.newNoty({type: "SUCCESS", title, message}) },
     delNoty(id: number) {
       this.TOASTS = this.TOASTS.filter((item: ToastType) => item.id !== id)
     }
