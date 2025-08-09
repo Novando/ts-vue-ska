bd04d81749e5feae2ca8adb8f53c3b60468e4456
novando <rizkynovando@gmail.com>
2025-08-09 22:03:57 +0700

    feature: atomic design system

---
+++ src/components/molecules/SampleFormMolecule.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+import SampleButtonAtomic from "../atomics/SampleButtonAtomic.vue";
+</script>
+
+<template>
+  <section class="flex flex-col">
+    <p>A million miles away from proxy centaury, a button is born</p>
+    <SampleButtonAtomic />
+  </section>
+</template>
