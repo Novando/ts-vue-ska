f85684e0c6809f9bf5e7b3d0422c83f6d7caec9c
novando <rizkynovando@gmail.com>
2025-08-21 15:54:33 +0700

    feature: use composition for toaster

--- src/stores/toaster.store.ts
+++ src/stores/toaster.store.ts
@@ -1,40 +1,33 @@
 import {defineStore} from "pinia"
+import { computed, ref } from "vue"
 
 type NewToastArgType = {
   type: "ERR"|"WARN"|"INFO"|"SUCCESS"
-  title: string
-  message?: any
+  message: string
 }
 type ToastType = NewToastArgType & {
   id: number
 }
-type StateType = {
-  TOASTS: ToastType[]
-}
 
-export const useToaster = defineStore("toaster", {
-  state: (): StateType => ({
-    TOASTS: [],
-  }),
-  getters: {
-    toasts: (s) => s.TOASTS,
-  },
-  actions: {
-    newNoty(arg: NewToastArgType) {
-      const id = new Date().getTime()
-      if (typeof arg.message === "object") arg.message = Object.values(arg.message)[0]
-      if (this.TOASTS.length > 2) this.TOASTS.shift()
-      this.TOASTS.push({...arg, id})
-      setTimeout(() => {
-        this.TOASTS = this.TOASTS.filter((item) => item.id !== id)
-      }, 3000)
-    },
-    notyErr(title: string, message?: any) { this.newNoty({type: "ERR", title, message}) },
-    notyWarn(title: string, message?: any) { this.newNoty({type: "WARN", title, message}) },
-    notyInfo(title: string, message?: any) { this.newNoty({type: "INFO", title, message}) },
-    notySuccess(title: string, message?: any) { this.newNoty({type: "SUCCESS", title, message}) },
-    delNoty(id: number) {
-      this.TOASTS = this.TOASTS.filter((item: ToastType) => item.id !== id)
-    }
+export const useToaster = defineStore("toaster", () => {
+  const toaster = ref<ToastType[]>([])
+  const toasts = computed(() => toaster.value)
+
+  function newNoty(arg: NewToastArgType) {
+    const id = new Date().getTime()
+    if (typeof arg.message === "object") arg.message = Object.values(arg.message)[0] as string
+    if (toaster.value.length > 2) toaster.value.shift()
+    toaster.value.push({...arg, id})
+    setTimeout(() => {
+      toaster.value = toaster.value.filter((item) => item.id !== id)
+    }, 3000)
   }
+
+  function notyErr(message: string) { newNoty({type: "ERR", message}) }
+  function notyWarn(message: string) { newNoty({type: "WARN", message}) }
+  function notyInfo(message: string) { newNoty({type: "INFO", message}) }
+  function notySuccess(message: string) { newNoty({type: "SUCCESS", message}) }
+  function delNoty(id: number) { toaster.value = toaster.value.filter((item: ToastType) => item.id !== id) }
+
+  return { toasts, notyErr, notyWarn, notyInfo, notySuccess, delNoty }
 })
